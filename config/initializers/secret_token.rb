cda0ccdcc9f1097e20e3b9f77465ef46a5430404
Steel <steel@steel-VirtualBox.(none)>
2013-05-09 21:27:33 +0530

    Updated rails app, added TwitterBootstrap and updated statuses.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'ed9bdbe6fca9e7fa864bb3633e35d62cbbd5df85b1484b27c2264ee52f976bbc234a184e71a825adc3049fce84b24cf0657a2e4c091d714388eec7542e6193a0'
